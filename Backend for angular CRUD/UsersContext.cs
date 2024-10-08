c899c66260aafd972b93657ca2e9b274deba0f4b
Luigi9 <fastromankudrik@gmail.com>
2024-10-08 22:54:30 +0500

    Installed EFCore; Started UsersContect(EFCORE);

---
+++ Backend for angular CRUD/UsersContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Backend_for_angular_CRUD.Model;
+public class UsersContext: DbContext
+{
+	public DbSet<User> Users { get; set; }
+}
