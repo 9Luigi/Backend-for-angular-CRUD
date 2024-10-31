aeef136b08610778490860f5dc96f615ee1eda5d
Luigi9 <fastromankudrik@gmail.com>
2024-10-31 21:42:58 +0500

    minor; added comments

--- Backend for angular CRUD/Program.cs
+++ Backend for angular CRUD/Program.cs
@@ -7,14 +7,14 @@ public class Program
 {
 	public static void Main(string[] args)
 	{
-		List<User> usersToPost = new List<User>() {
+		List<User> usersToPost = new List<User>() { //set to init db
 			new User("Roman","Kudrik",26),
 			new User("Vladimir","Sadkov",26),
 			new User("Alexandra","Kravchenko",26),
 			new User("Galina","Maizlina",21),
 			new User("Dmitry","Surkov",29)
 		};
-		var usersContext = new UsersContext();
+		var usersContext = new UsersContext(); //dbContext for users model
 		var cRUDController = new CRUDController<User, UsersContext>(usersContext);
 
 		var builder = WebApplication.CreateBuilder();
@@ -22,19 +22,19 @@ public class Program
 
 
 		var app = builder.Build();
-		app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+		app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //cross domen for different dns front/backend
 		app.MapGet("/AddList", async () =>
 		{
-			await cRUDController.ADD(usersToPost.ToArray());
+			await cRUDController.ADD(usersToPost.ToArray()); //initiate db via route
 		});
 		app.MapGet("/", async (HttpResponse response) =>
 		{
 			response.ContentType = "application/json";
 			var result = await cRUDController.SelectAsync();
 			System.Diagnostics.Debug.WriteLine("All users send");
-			return result;
+			return result; //return full dbSet
 		});
-		app.MapGet("/api/users/{id}", async (HttpResponse response, string id) =>
+		app.MapGet("/api/users/{id}", async (string id) =>
 		{
 			User? userToFind;
 			List<User> users = await cRUDController.SelectAsync(id);
@@ -42,16 +42,16 @@ public class Program
 			if(userToFind != null)
 			{
 				System.Diagnostics.Debug.WriteLine(userToFind!.Name + ": Send");
-				return userToFind;
+				return userToFind; //return user from dbSet
 			}
 			else
 			{
-				return null;
+				return null; //handled in CRUDController
 			}
 		});
 		app.MapDelete("/api/users/{id}", async (string id) =>
 		{
-			await cRUDController.Remove(id);
+			await cRUDController.Remove(id); //remove user record from table in database
 			System.Diagnostics.Debug.WriteLine("User with id:" + id + " Removed");
 		});
 		app.MapPost("api/users/", async (HttpRequest request) =>
@@ -59,17 +59,20 @@ public class Program
 			User? sentUser = await request.ReadFromJsonAsync<User>();
 			if (sentUser != null)
 			{
-				sentUser.Id = Guid.NewGuid().ToString();
-				await cRUDController.ADD(sentUser);
+				sentUser.Id = Guid.NewGuid().ToString(); //generate new user's uniq id
+				await cRUDController.ADD(sentUser); //Create and push user's entity to database
 			}
 		});
 
 
 		app.MapPut("/api/users/", async (HttpRequest request) =>
 		{
-			User? sendUser = await request.ReadFromJsonAsync<User>();
-			User? foundUser = usersContext.Users.First(x => x.Id == sendUser!.Id);
-			await cRUDController.AttachUpdate(sendUser!, foundUser!);
+			User? sendUser = await request.ReadFromJsonAsync<User>(); //attempt to read user's entity from resieved json
+			if(sendUser != null)
+			{
+				User? foundUser = usersContext.Users.First(x => x.Id == sendUser!.Id); //if attempt above succed search that entity in database
+				await cRUDController.AttachUpdate(sendUser!, foundUser!); //change all edited(don't matched) fields via refliction in CRUDController
+			}
 		});
 		app.Run();
 	}
--- Backend for angular CRUD/Services/CRUDController/CRUDController.cs
+++ Backend for angular CRUD/Services/CRUDController/CRUDController.cs
@@ -14,10 +14,10 @@ namespace Backend_for_angular_CRUD
 			switch (entities.Length)
 			{
 				case 1:
-					context.Add(entities[0]);
+					context.Add(entities[0]); //if 1 argument send will be add 1 entity in db
 					break;
 				default:
-					context.AddRange(entities);
+					context.AddRange(entities);//if >1 argument send will be add all sended entities
 					break;
 			}
 			try
@@ -26,7 +26,6 @@ namespace Backend_for_angular_CRUD
 			}
 			catch (Exception ex) { throw new Exception("Cannot save entity/es via CRUDController ADD method", ex); }
 		}
-		//TODO to think send User object or id as already send
 		protected internal async Task<List<T>> SelectAsync(params object[] ids)
 		{
 			var result = new List<T>();
@@ -34,7 +33,7 @@ namespace Backend_for_angular_CRUD
 			{
 				if (ids.Length == 0)
 				{
-					result = await context.Set<T>().ToListAsync();
+					result = await context.Set<T>().ToListAsync();//if 0 arguments send return all dbset(all records in db)
 				}
 				foreach (var id in ids)
 				{
@@ -44,22 +43,21 @@ namespace Backend_for_angular_CRUD
 						result.Add(entity);
 					}
 				}
-				return result;
+				return result; //if >=1 arguments send return entitys that contains send id/id's
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex);
-				return new List<T>();
+				return new List<T>(); //cause don't return null
 			}
 		}
-
 		protected internal async Task Remove(params string[] ids)
 		{
 			T? entityToRemove;
 			switch (ids.Length)
 			{
-				case 0: throw new ArgumentException("No user IDs provided.");
-				case 1:
+				case 0: throw new ArgumentException("No user IDs provided.");//if method invoke with no arguments, logic dosen't let this happen, cause something have to be deleted
+				case 1: //remove 1 record
 					entityToRemove = await context.Set<T>().FindAsync(ids[0]);
 					if (entityToRemove != null)
 					{
@@ -67,10 +65,10 @@ namespace Backend_for_angular_CRUD
 						await context.SaveChangesAsync();
 					}
 					break;
-				default:
+				default://remove all record send as arguments
 					var entitiesToRemove = new List<T>();
 					entitiesToRemove = await context.Set<T>().Where(e => ids.Contains((string)e.GetType().GetProperty("Id")!.GetValue(e)!))
-											 .ToListAsync();
+											 .ToListAsync();//get value of Id fields via reflection cause class uses generic and compiller sees error
 					if (entitiesToRemove.Count > 0)
 					{
 						context.RemoveRange(entitiesToRemove);
@@ -79,7 +77,7 @@ namespace Backend_for_angular_CRUD
 					break;
 			}
 		}
-		protected internal async Task AttachUpdate(T sendEntity, T foundEntity)
+		protected internal async Task AttachUpdate(T sendEntity, T foundEntity) //changes edited fields in entity and push it into db
 		{
 			context.Attach(foundEntity);
 			FieldsController.CopyFields<T>(sendEntity, foundEntity);
